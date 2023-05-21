7a63f648d1b90a454c92bc14303d10ad7406cf8d
NESTARS <66269490+nestor5toglan@users.noreply.github.com>
2023-05-21 14:28:14 +0000

    Update and rename index.html to index.php
    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <p>ok bonjour didi!</p>
 <p>je pense que ça doit aller avec ce si,ple fichier index.</p>
 <p>cest un domaine de github et cest heberger sur github!jespere que ça fera laffaire!</p>
-
+<?php echo hello1; ?>
+<?php echo tx_reference; ?>
+<?php echo hello2; ?>
 </body>
 </html>
